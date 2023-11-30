3422eca987c2702b95c7919ecab8598fb6904414
pramudiindeesha <wpramudiindeesha@gmail.com>
2023-11-30 10:41:43 +0530

    database function changes

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,21 @@
 const server  = require ("./server.js");
-
-if(process.env.NODE_ENV === "test"){
+const PORT = process.env.NODE_ENV === "test" ? 4401 : 8080;
+  server.listenPORT, () => { 
+    console.log(
+      "Capstone Project Backend is running on http://localhost:${PORT"
+      );
+}else{
   server.listen(3000, () => {
     console.log(
       "Capstone Project Backend is running on http://localhost:3000"
     );
-  });
+  })
 }else{
   server.listen(8080, () => {
     console.log(
       "Capstone Project Backend is running on http://localhost:8080"
     );
-  });
+  })
 }
 
-
-const server = require ("./server.js");
- const PORT = process.env.NODE_ENV === "test" ? 4401 : 8080;
-  server.listen(PORT, () => { 
-    console.log(
-      Capstone Project Backend is running on http://localhost:${PORT}
-      ); 
-    }); 
-
-
+ 
